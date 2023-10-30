37c76130c3ed00e1f42a879f573d2d6b04c403aa
aninha <anamarianevesbatista@gmail.com>
2023-10-30 06:40:05 -0300

    🌟 feat: Novo ex tipos primitivos

--- app/ex02/index.php
+++ app/ex02/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,11 +10,34 @@
 
 <body>
     <section class="text-center items-center justify-center mx-20">
-        <h1>Exemplo de PHP</h1>
+        <h1 class="text-bold text-2xl  mt-20">Tipo primitivos PHP</h1>
+
         <?php
-        date_default_timezone_set("America/Sao_Paulo");
-        echo "Hoje é dia " . date("d/M/Y");
-        echo " e a hora atual é " . date("G:i:s T");
+        // 0x = hexadeciamal 0b = binário 0 = Octal
+        // $num = 0x1A;
+        // echo "O valor da variável é $num";
+
+        // $v  = true;
+        // var_dump(($v));
+
+        // $m = 3e2;
+        // echo "$m";
+
+        // $num = (integer) ("950");
+        // var_dump( $num );
+
+        // $casado = true;
+        // print"O valor para casado é $casado";
+
+        // $vet = [2, 3.8, 5, 6, 7, "nissan", 0];
+        // var_dump($vet)
+
+        // class Pessoa
+        // {
+        //            private $nome;
+        //  }
+        //   $p = new Pessoa;
+        //  var_dump($p);
         ?>
     </section>
 </body>
